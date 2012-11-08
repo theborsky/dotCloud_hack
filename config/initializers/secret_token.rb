be66f763a9e4c46febbd51c0fa97d12d1e44be14
Shawn Borsky <shawn@theborsky.com>
2012-11-07 19:47:04 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DotCloudHack::Application.config.secret_token = '4ab455cb2ed1e3802eceb7bebf993eb50a75cd2f7f6f9a158d9e79cb05e92ee5a83b4375302396a0610f29168278c2b5e3b130c991edd46b52aa823a61c04540'
